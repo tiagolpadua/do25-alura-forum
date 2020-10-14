87841ef251ade744844d013e2dc1ce42b18ed71d
F9342853 Tiago Lage Payne de Padua <tiagolpadua@bb.com.br>
2020-10-14 20:32:53 -0300

    ex-4.9

--- src/br/com/alura/forum/config/JPAConfiguration.java
+++ src/br/com/alura/forum/config/JPAConfiguration.java
@@ -31,27 +31,25 @@ public class JPAConfiguration {
 	}
 
 	/* DATASOURCE DE DESENVOLVIMENTO */ 
-	@Bean
-	public DataSource dataSource() {
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://localhost:3306/alura_forum?useSSL=false");
-		dataSource.setUsername("root");
-		dataSource.setPassword("");
-		return dataSource;
-	}
+//	@Bean
+//	public DataSource dataSource() {
+//		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+//		dataSource.setUrl("jdbc:mysql://localhost:3306/alura_forum?useSSL=false");
+//		dataSource.setUsername("root");
+//		dataSource.setPassword("");
+//		return dataSource;
+//	}
 
-	/* DATASOURCE DE PRODUCAO
 	@Bean
 	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://192.168.56.110:3306/alura_forum?useSSL=false");
+		dataSource.setUrl("jdbc:mysql://192.168.0.8:3306/alura_forum?useSSL=false");
 		dataSource.setUsername("alura");
 		dataSource.setPassword("qwerty123");
 		return dataSource;
 	}
-	*/
 	
 	@Bean
 	public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
